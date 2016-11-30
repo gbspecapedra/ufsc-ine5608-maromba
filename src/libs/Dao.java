51eb4bedb1e42409a8e5f220b539c3a96c190d6f
Orlando <orlando.baptista@grad.ufsc.br>
2016-11-30 20:35:13 -0200

    Versão configurada para a base externa (operacional)

--- src/libs/Dao.java
+++ src/libs/Dao.java
@@ -13,35 +13,25 @@ public class Dao {
 
     //  CONFIGURAÇÃO DO BANCO DE DADOS
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    //  static final String DB_URL = "jdbc:mysql://192.168.79.65/cellfix?useSSL=false";
-    //  static final String DB_URL = "jdbc:mysql://150.162.79.1:1404/cellfix?useSSL=false";
-//    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/cellfix?useSSL=false";
-    static final String DB_URL = "jdbc:mysql://msxacademic.mysql.dbaas.com.br:3306/msxacademic?useSSL=false";
-//    static final String DB_URL = "jdbc:mysql://192.168.79.32:3306/s02?useSSL=false";
-    
-            
-//    static final String USER = "root";
-//    static final String PASS = "zorariver";
-    
-    static final String USER = "msxacademic";
-    static final String PASS = "underwood";
-    
-//     static final String USER = "sa";
-//    static final String PASS = "fithos0932";
-    
+
+    // static final String DB_URL = "jdbc:mysql://msxacademic.mysql.dbaas.com.br:3306/msxacademic?useSSL=false";
+    // static final String USER = "msxacademic";
+    // static final String PASS = "underwood";
+    static final String DB_URL = "jdbc:mysql://150.162.79.1:3306/s02?useSSL=false";
+    static final String USER = "sa";
+    static final String PASS = "fithos0932";
 
     private void conecta() throws SQLException {
-//        System.out.println("Conectando...");
+        // System.out.println("Conectando...");
         this.conexao = DriverManager.getConnection(DB_URL, USER, PASS);
         this.statement = this.conexao.createStatement();
-//        System.out.println("Conexão efetuada com sucesso");
+        // System.out.println("Conexão efetuada com sucesso");
     }
 
     public ResultSet select(String query) throws SQLException {
         this.conecta();
-        ResultSet resultados = statement.executeQuery(query);        
+        ResultSet resultados = statement.executeQuery(query);
         return resultados;
-        
     }
 
     public void execute(String query) throws SQLException {
@@ -65,7 +55,5 @@ public class Dao {
     public void setStatement(Statement statement) {
         this.statement = statement;
     }
-    
-    
-    
+
 }
